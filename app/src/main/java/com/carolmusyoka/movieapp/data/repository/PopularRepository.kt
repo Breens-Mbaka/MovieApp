47a5cb1f308d25fcd7de7ad0254ab3096359a1c7
Samurai <44951692+carolinemusyoka@users.noreply.github.com>
2021-07-12 11:42:37 +0300

    refactor_0.01

--- app/src/main/java/com/carolmusyoka/movieapp/data/repository/PopularRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.carolmusyoka.movieapp.data.repository
-
-import com.carolmusyoka.movieapp.data.api.ApiHelper
-
-class PopularRepository (private val apiHelper: ApiHelper) {
-    suspend fun getPopularMovies() = apiHelper.getPopularMovies()
-}
